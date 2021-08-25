cde08c15ab358889c68b4a20344b0ad5877dbd20
Ranji02 <66516169+Ranji02@users.noreply.github.com>
2021-08-25 21:59:24 +0530

    Problem1.py added

---
+++ Getting Started With Competitive Programming/WEEK0/Programming Assignment/Problem1.py
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int T;
+  cin>>T;
+  for(int i=0;i<T;i++)
+  {
+    int num1,num2;
+    cin>>num1>>num2;
+    cout<<num1+num2<<endl;
+  }
+  return 0;
+}
